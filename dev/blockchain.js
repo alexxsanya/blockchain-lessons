088274bd44b8321ae1bf5de81875e658efe28e07
alexxsanya <alexxsanya@gmail.com>
2021-01-05 00:52:31 +0300

    rename newTransaction object to pendingTransaction

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,13 +1,13 @@
 function Blockchain(){
     this.chain = [];  // all the blocks will be stored in here
-    this.newTransaction = []; // hold new transaction that a created before they are mined
+    this.pendingTransaction = []; // hold new transaction that a created before they are mined/validated
 }
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash){
     const newBlock = {
         index: this.chain.length + 1, //basically the block number in the chain,
         timestamp: Date.now(), //when the block was created
-        transactions: this.newTransaction, //all the transactions that waiting to be placed into a block
+        transactions: this.pendingTransaction, //all the transactions that waiting to be placed into a block
         nonce: nonce, //comes from a proof of work....basically a number that we created this new block in a legitimate way
         hash: hash, // is the hash value of the transaction data of the current block
         previousBlockHash: previousBlockHash // equals to the hash value of the previous block
@@ -21,6 +21,16 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash){
 
 Blockchain.prototype.getLastBlock = function(){
     return this.chain[this.chain.length - 1];
+}
+
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient){
+    const newTransaction = {
+        amount: amount,
+        sender: sender,
+        recipient: recipient
+    }
+
+    this.pendingTransaction.push(newTransaction);
 }
 
 
