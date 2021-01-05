9acd482c0923e6a9e69421c0c9cdf7875e4eb5cb
alexxsanya <alexxsanya@gmail.com>
2021-01-05 23:25:35 +0300

    add blockchain api foundation

---
+++ dev/api.js
@@ -0,0 +1,33 @@
+/**
+ *  :::::: API SERVER ::::::
+ *  Which interacts blockchain data structure, 
+ *  To fetch an entire blockchain, create new transactions, to mine blocks
+ * 
+*/
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// fetch entire blockchain
+app.get('/blockchain', function(req, res){
+
+});
+
+
+//create a new transaction on the blockchain
+app.post('/transaction', function(req, res){
+    res.send(req.body)
+});
+
+// create or mine a new block
+app.get('/mine', function(req, res){
+
+})
+
+app.listen(4600, function(){
+    console.log('>>> Listening on port 4600 ')
+});
