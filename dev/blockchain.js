53969a69f9b4abefbb9db623d1064154fca10fbc
alexxsanya <alexxsanya@gmail.com>
2021-01-06 00:38:26 +0300

    fix misspellings

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -2,7 +2,7 @@ const sha256 =require('sha256');
 
 function Blockchain(){
     this.chain = [];  // all the blocks will be stored in here
-    this.pendingTransaction = []; // hold new transaction that a created before they are mined/validated
+    this.pendingTransactions = []; // hold new transaction that a created before they are mined/validated
 
     /**
      * Creating the Genesis Block
@@ -18,13 +18,14 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash){
     const newBlock = {
         index: this.chain.length + 1, //basically the block number in the chain,
         timestamp: Date.now(), //when the block was created
-        transactions: this.pendingTransaction, //all the transactions that waiting to be placed into a block
+        transactions: this.pendingTransactions, //all the transactions that waiting to be placed into a block
         nonce: nonce, //comes from a proof of work....basically a number that we created this new block in a legitimate way
         hash: hash, // is the hash value of the transaction data of the current block
         previousBlockHash: previousBlockHash // equals to the hash value of the previous block
     };
 
-    this.previousBlockHash = []; //empty the prev for next transaction
+    console.log(newBlock)
+    this.pendingTransactions = []; //empty the prev for next transaction
     this.chain.push(newBlock); //add the new block to the chain
 
     return newBlock;
@@ -41,7 +42,7 @@ Blockchain.prototype.createNewTransaction = function(amount, sender, recipient){
         recipient: recipient
     }
 
-    this.pendingTransaction.push(newTransaction);
+    this.pendingTransactions.push(newTransaction);
     
     //this returns the block number that this transaction will be added to
     return this.getLastBlock()['index'] + 1
