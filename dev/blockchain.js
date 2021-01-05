30b5936dccac3de9e564f2e935c96c237afb9fbd
alexxsanya <alexxsanya@gmail.com>
2021-01-05 19:33:47 +0300

    add proof of work descriptions

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -46,5 +46,22 @@ Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, n
 
 }
 
+Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData){
+    /**
+     * 1 => repeatedly has a block until we find a correct has => starting with 4 zero => '0000123MDFORNMFDJFD'
+     * this is achieved by repeated chaning the nonce in hashBlock(previousBlockHash, currentBlockData, nonce)
+     * Then return the nonce value that creates the correct hash
+     * 
+     * How this secures the block chain
+     * - Remeber to generate the desired hash starting with the 4 zero 
+     * we are going to generate the hash over 10s of thousand time to get one
+     * which will use a lot of computing power.
+     * Therefore, if an individual wats to modify the previous entered bloc, one will have to try regenerate
+     * the hash which will take lots of time & energy -> which is not feasible
+     * And since the blocks contain a previous hash, that would mean one will need to do a proof of work for all previous block which is not feasible
+     */
+
+     
+}
 
 module.exports = Blockchain;
