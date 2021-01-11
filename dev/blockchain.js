5d8acc6b78d73a3f74a476bc786500ff93dedb97
alexxsanya <alexxsanya@gmail.com>
2021-01-11 23:29:05 +0300

    add code to validate the genesis block

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -105,12 +105,28 @@ Blockchain.prototype.chainIsValid = function(blockchain) {
     for(var i=1; i < blockchain.length; i++) {
         const currentBlock = blockchain[i];
         const prevBlock = blockchain[i - 1];
+        const currentBlockHash = this.hashBlock(prevBlock['hash'], 
+        {
+            transactions: currentBlock['transactions'],
+            index: currentBlock['index']
+        }, currentBlock['nonce']);
+        if(currentBlockHash.substring(0, 4) !== '0000') validChain = false;
 
         if(currentBlock['previousBlockHash'] !== prevBlock['hash']){
             //then the chain is not valid
             validChain = false;
         }
     }
+
+    //check our genesis block is valid based on the values we used to create it
+    const genesisBlock = blockchain[0]
+    const correctNonce = genesisBlock['nonce'] === 200;
+    const correctPreviousBlockHash = genesisBlock['previousBlockHash'] === '0';
+    const correctHash = genesisBlock['hash'] === '0';
+    const correctTransactions = genesisBlock['transactions'].length === 0
+
+    if(!correctNonce || !correctPreviousBlockHash || !correctHash || !correctTransactions) validChain = false;
+
 
     return validChain;
 }
