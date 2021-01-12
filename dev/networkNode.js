14d4e1103e59bf580530d4cafb3867880a77775f
alexxsanya <alexxsanya@gmail.com>
2021-01-13 00:28:19 +0300

    fix block-explorer endpoint

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -338,8 +338,9 @@ app.get('/address/:address', function(req, res){
 
 
 app.get('/block-explorer', function(req, res){
-    res.sendFile('./blocker-explorer/index.html', {root: __dirname});
-})
+    res.sendFile('./block-explorer/index.html', {root: __dirname});
+});
+
 app.listen(port, function(){
     console.log(`>>> Listening on port ${port} `)
 });
